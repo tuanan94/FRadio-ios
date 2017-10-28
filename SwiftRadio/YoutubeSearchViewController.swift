865a754041ceb8be44270f56bf2f314e1635cbc7
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2017-10-29 03:54:00 +0900

    Tmp

--- SwiftRadio/YoutubeSearchViewController.swift
+++ SwiftRadio/YoutubeSearchViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class YoutubeSearchViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var dataSource: MyDataSource?
+    var delegate: MyDataDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = MyDataSource()
+        delegate = MyDataDelegate()
         tableView.dataSource = dataSource
+        tableView.delegate = delegate
         tableView.reloadData()
         super.viewDidLoad()
     }
@@ -37,6 +40,7 @@ class YoutubeSearchViewController: UIViewController {
 }
 
 class MyDataSource: NSObject, UITableViewDataSource{
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
@@ -45,9 +49,11 @@ class MyDataSource: NSObject, UITableViewDataSource{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Youtube-cell", for: indexPath) as! YoutubeSearchTableViewCell
         return cell
     }
-    
-    
-    
 }
 
+class MyDataDelegate: NSObject, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
 
